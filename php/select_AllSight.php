9a25a311f7e999461e790d64b4080767d67574c0
Eric <0524eric@gmail.com>
2023-01-02 15:08:23 +0800

    更新

--- php/select_AllSight.php
+++ php/select_AllSight.php
@@ -1,26 +1,18 @@
 <?php    
 	include "db_conn.php";
 	
-	
-	$query = ("select * from sights");
+	$query = ("select * from sights ");
 	$stmt =  $db->prepare($query);
-	$error= $stmt->execute(array());
+	$error= $stmt->execute();
 	$result = $stmt->fetchAll();
 	//以上寫法是為了防止「sql injection」
-
-    $SightArray = array();
-	if(count($result)==0){   
-        echo "no sights";   
-    }
-    else{
-        for($i=0; $i<count($result); $i++){
-           
-            $SightObject =  array( 'SightName'  =>  $result[$i]['SightName'], 'Description'  =>  $result[$i]['Description'],
-                                     'Address'  =>  $result[$i]['Address'], 'Zone'  =>  $result[$i]['Zone'], 'Count'  =>  $result[$i]['Count'],
-                                     'Category'  =>  $result[$i]['Category'], 'PhotoURL'  =>  $result[$i]['PhotoURL']);
-            $SightArray[] = $SightObject;
-        }
-    }
-    header('Content-Type: application/json; charset=utf-8');
-    echo json_encode($SightArray, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-?>
+	$sights = array();
+	for($i=0; $i<count($result); $i++){
+		$sights[$i]=array("SightName" => $result[$i]['SightName'],"Description" => $result[$i]['Description'],
+		"Address"=>$result[$i]['Address'],"Zone"=>$result[$i]['Zone'],"Category"=>$result[$i]['Category'],
+		"PhotoURL"=>$result[$i]['PhotoURL'],"StarNum"=>$result[$i]['StarNum'],"Count"=>$result[$i]['Count']);	
+		
+	}
+	header('Content-Type: application/json; charset=utf-8');
+	echo json_encode($sights,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+	?>
--- php/select_sights.php
+++ php/select_sights.php
@@ -1,20 +1,19 @@
 <?php    
 	include "db_conn.php";
 	
-	$SightName = "海大";
+	$SightName = $_GET['SightName'];
 	$query = ("select * from sights where SightName = ?");
 	$stmt =  $db->prepare($query);
 	$error= $stmt->execute(array($SightName));
 	$result = $stmt->fetchAll();
 	//以上寫法是為了防止「sql injection」
-	
+	$sights = array();
 	for($i=0; $i<count($result); $i++){
-		echo "SightName:".$result[$i]['SightName'].' '.
-			 "Description:". $result[$i]['Description'].' '.
-			 "Address:".$result[$i]['Address'].' '.
-			 "Zone:". $result[$i]['Zone'].' '.
-			 "Category:". $result[$i]['Category'].' '.
-			 "PhotoURL:". $result[$i]['PhotoURL'].' '.
-			 '<br>'; 
+		$sights[$i]=array("SightName" => $result[$i]['SightName'],"Description" => $result[$i]['Description'],
+		"Address"=>$result[$i]['Address'],"Zone"=>$result[$i]['Zone'],"Category"=>$result[$i]['Category'],
+		"PhotoURL"=>$result[$i]['PhotoURL'],"StarNum"=>$result[$i]['StarNum'],"Count"=>$result[$i]['Count']);	
+		
 	}
+	header('Content-Type: application/json; charset=utf-8');
+	echo json_encode($sights,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 	?>
